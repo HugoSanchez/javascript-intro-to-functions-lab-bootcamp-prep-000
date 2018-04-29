c2519391e9fdadcae8e68bd3711053483eafefbc
Hugo Sanchez <hugosanchez251@gmail.com>
2018-04-29 19:24:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function sayHiToGrandma(string){
   }else if (string.toUpperCase === string){
     return 'YES INDEED!'
   }else if (string.toLowerCase === string){
-    return "I can't hear you!"
+    return "I can't hear you!!!!!!"
   }
 } 
